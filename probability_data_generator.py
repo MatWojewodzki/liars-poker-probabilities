0ba02a74c9ef7b9034c5e6c9b73df5c61e00a17d
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2025-03-22 21:03:54 +0100

    Remove outdated commented code.

--- probability_data_generator.py
+++ probability_data_generator.py
@@ -18,69 +18,6 @@ def main():
 
     selected_cards = np.arange(1, deck_info.deck_size)
 
-    # high_card_func = hand_probability.at_least_of_ranks_probability_function(1)
-    # pair_func = hand_probability.at_least_of_ranks_probability_function(2)
-    # three_of_a_kind_func = hand_probability.at_least_of_ranks_probability_function(3)
-    # four_of_a_kind_func = hand_probability.at_least_of_ranks_probability_function(4)
-    # two_high_cards_func = hand_probability.at_least_of_ranks_probability_function(1, 1)
-    # pair_and_high_card_func = hand_probability.at_least_of_ranks_probability_function(2, 1)
-    # two_pair_func = hand_probability.at_least_of_ranks_probability_function(2, 2)
-    # three_of_a_kind_and_high_card_func = hand_probability.at_least_of_ranks_probability_function(3, 1)
-    # full_house_func = hand_probability.at_least_of_ranks_probability_function(3, 2)
-    #
-    # street_of_5_func = hand_probability.at_least_of_ranks_probability_function(*repeat(1, 5))
-    # street_of_4_func = hand_probability.at_least_of_ranks_probability_function(*repeat(1, 4))
-    # street_of_3_func = hand_probability.at_least_of_ranks_probability_function(*repeat(1, 3))
-    # street_of_2_func = hand_probability.at_least_of_ranks_probability_function(*repeat(1, 2))
-    # street_of_1_func = hand_probability.at_least_of_ranks_probability_function(*repeat(1, 1))
-    #
-    # flush_of_5_func = hand_probability.at_least_of_suits_probability_function(5)
-    # flush_of_4_func = hand_probability.at_least_of_suits_probability_function(4)
-    # flush_of_3_func = hand_probability.at_least_of_suits_probability_function(3)
-    # flush_of_2_func = hand_probability.at_least_of_suits_probability_function(2)
-    # flush_of_1_func = hand_probability.at_least_of_suits_probability_function(1)
-    #
-    # straight_flush_of_5_func = hand_probability.unique_cards_probability_function(5)
-    # straight_flush_of_4_func = hand_probability.unique_cards_probability_function(4)
-    # straight_flush_of_3_func = hand_probability.unique_cards_probability_function(3)
-    # straight_flush_of_2_func = hand_probability.unique_cards_probability_function(2)
-    # straight_flush_of_1_func = hand_probability.unique_cards_probability_function(1)
-    #
-    # probability_data = {
-    #     "rank": {
-    #         "1": list_of_probabilities(selected_cards, high_card_func),
-    #         "2": list_of_probabilities(selected_cards, pair_func),
-    #         "3": list_of_probabilities(selected_cards, three_of_a_kind_func),
-    #         "4": list_of_probabilities(selected_cards, four_of_a_kind_func),
-    #         "1+1": list_of_probabilities(selected_cards, two_high_cards_func),
-    #         "2+1": list_of_probabilities(selected_cards, pair_and_high_card_func),
-    #         "2+2": list_of_probabilities(selected_cards, two_pair_func),
-    #         "3+1": list_of_probabilities(selected_cards, three_of_a_kind_and_high_card_func),
-    #         "3+2": list_of_probabilities(selected_cards, full_house_func)
-    #     },
-    #     "street": {
-    #         "5": list_of_probabilities(selected_cards, street_of_5_func),
-    #         "4": list_of_probabilities(selected_cards, street_of_4_func),
-    #         "3": list_of_probabilities(selected_cards, street_of_3_func),
-    #         "2": list_of_probabilities(selected_cards, street_of_2_func),
-    #         "1": list_of_probabilities(selected_cards, street_of_1_func)
-    #     },
-    #     "flush": {
-    #         "5": list_of_probabilities(selected_cards, flush_of_5_func),
-    #         "4": list_of_probabilities(selected_cards, flush_of_4_func),
-    #         "3": list_of_probabilities(selected_cards, flush_of_3_func),
-    #         "2": list_of_probabilities(selected_cards, flush_of_2_func),
-    #         "1": list_of_probabilities(selected_cards, flush_of_1_func)
-    #     },
-    #     "straight_flush": {
-    #         "5": list_of_probabilities(selected_cards, straight_flush_of_5_func),
-    #         "4": list_of_probabilities(selected_cards, straight_flush_of_4_func),
-    #         "3": list_of_probabilities(selected_cards, straight_flush_of_3_func),
-    #         "2": list_of_probabilities(selected_cards, straight_flush_of_2_func),
-    #         "1": list_of_probabilities(selected_cards, straight_flush_of_1_func)
-    #     }
-    # }
-
     probability_data = {
         "rank": {
             f"{i}+{j}": list_of_probabilities(selected_cards, hand_probability.at_least_of_ranks_probability_function(i, j))
