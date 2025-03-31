527fcc558c121be6377c1a069663dce975c9eccd
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2025-03-31 18:25:24 +0200

    Redo the JSON generation logic.
    The file now contains valid data about the probability of poker hands when some of the cards on our own hand already match with them. Removed redundant use of numpy.

--- probability_data_generator.py
+++ probability_data_generator.py
@@ -1,43 +1,87 @@
-from hand_probability import DeckInfo, HandProbability
-import numpy as np
-from itertools import repeat, product
+# Script generating a JSON file containing the probability of all poker hands in the liar's poker card game
+# taking into account cards on your own hand that already match a specified poker hand.
+
+from hand_probability import DeckInfo, HandProbability, CardRequirement
+from itertools import repeat
 import json
 
-
 OUTPUT_FILE_NAME: str = "probability_data.json"
-DIGITS_OF_PRECISION: int | None = 6
+DIGITS_OF_PRECISION: int | None = 4
 PRETTIFY: bool = False
 
 
-def list_of_probabilities(selected_cards: np.ndarray, func: callable):
-    return np.vectorize(func)(selected_cards).tolist()
+def list_of_probabilities(func: callable):
+    return [func(i) for i in range(1, 24)]
 
 
 def main():
     deck_info = DeckInfo(deck_size=24, cards_of_rank_count=4, cards_of_suit_count=6)
     hand_probability = HandProbability(deck_info, digits_of_precision=DIGITS_OF_PRECISION)
 
-    selected_cards = np.arange(1, deck_info.deck_size)
-
     probability_data = {
-        "rank": {
-            f"{i}+{j}": list_of_probabilities(selected_cards, hand_probability.at_least_of_ranks_probability_function(i, j))
-            for i, j in product(range(1, 4), range(0, 3)) if i >= j
-        } | {
-            "4+0": list_of_probabilities(selected_cards, hand_probability.at_least_of_ranks_probability_function(4))
-        },
-        "straight": {
-            str(i): list_of_probabilities(selected_cards, hand_probability.at_least_of_ranks_probability_function(*repeat(1, i)))
-            for i in range(1, 6)
-        },
-        "flush": {
-            str(i): list_of_probabilities(selected_cards, hand_probability.at_least_of_suits_probability_function(i))
-            for i in range(1, 6)
-        },
-        "straight_flush": {
-            str(i): list_of_probabilities(selected_cards, hand_probability.unique_cards_probability_function(i))
-            for i in range(1, 6)
-        }
+        "highCard": list_of_probabilities(
+            hand_probability.create_probability_func(CardRequirement(1, 4))
+        ),
+        "pair": [
+            list_of_probabilities(
+                hand_probability.create_probability_func(CardRequirement(2 - matched_cards, 4 - matched_cards))
+            )
+            for matched_cards in range(2)
+        ],
+        "threeOfAKind": [
+            list_of_probabilities(
+                hand_probability.create_probability_func(CardRequirement(3 - matched_cards, 4 - matched_cards))
+            )
+            for matched_cards in range(3)
+        ],
+        "fourOfAKind": [
+            list_of_probabilities(
+                hand_probability.create_probability_func(CardRequirement(4 - matched_cards, 4 - matched_cards))
+            )
+            for matched_cards in range(4)
+        ],
+        "twoPair": [[
+            list_of_probabilities(
+                hand_probability.create_probability_func(
+                    CardRequirement(2 - matched_first_cards, 4 - matched_first_cards),
+                    CardRequirement(2 - matched_second_cards, 4 - matched_second_cards)
+                )
+            )
+            for matched_second_cards in range(matched_first_cards + 1)
+            if matched_second_cards != 2  # case 2+2 is always 100% probable
+        ]
+            for matched_first_cards in range(3)
+        ],
+        "fullHouse": [[
+            list_of_probabilities(
+                hand_probability.create_probability_func(
+                    CardRequirement(2 - matched_first_cards, 4 - matched_first_cards),
+                    CardRequirement(2 - matched_second_cards, 4 - matched_second_cards)
+                )
+            )
+            for matched_second_cards in range(3)
+            if not (matched_first_cards == 3 and matched_second_cards == 2)  # case 3+2 is always 100% probable
+        ]
+            for matched_first_cards in range(4)
+        ],
+        "straight": [
+            list_of_probabilities(
+                hand_probability.at_least_of_ranks_probability_function(*repeat(1, 5 - matched_cards))
+            )
+            for matched_cards in range(5)
+        ],
+        "flush": [
+            list_of_probabilities(
+                hand_probability.create_probability_func(CardRequirement(5 - matched_count, 6 - matched_count))
+            )
+            for matched_count in range(5)
+        ],
+        "straightFlush": [
+            list_of_probabilities(
+                hand_probability.unique_cards_probability_function(5 - matched_count),
+            )
+            for matched_count in range(5)
+        ]
     }
 
     indent = 4 if PRETTIFY else None
